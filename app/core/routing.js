4aa7e6a05734521ebea5c8a0f16d0f52118c5dbe
ArthurMialon <arthurmialon@gmail.com>
2015-05-06 14:07:56 +0200

    new routing.js (Express router)

--- app/core/routing.js
+++ app/core/routing.js
@@ -1,3 +1,35 @@
-module.exports = function(router, requester, io) {
-	console.log(route);
+var Middleware    = require('../middleware/middleware');
+var Parameters    = require('./parameters');
+var Need    	  = require('./need');
+var Socket    	  = require('./socket');
+
+module.exports = function(router, route, io) {
+
+	router[route.method](route.path, route.middlewares, function(req, res, next) {
+
+		// HERE Check necessary parameters 
+
+		// HERE Check need
+
+		// HERE Launch before route
+
+		// Add route infos in the request
+		req.routeInfos = route;
+
+		// Launch the right method from the right controller
+		if(route.controller) {
+			route.controller[route.action](req, res, function(req, res) {
+				// Callback next
+				// HERE After route
+				// HERE Socket
+			});
+		}else {
+			route.action(req, res, function(req, res) {
+				// Callback next
+				// HERE After route
+				// HERE Socket				
+			});
+		}				
+
+	});	
 }
