abbe4eba9e25d9b74db4428b361294e2d6ebf3da
ArthurMialon <arthurmialon@gmail.com>
2015-05-06 12:15:03 +0200

    update route

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,4 +1,4 @@
- module.exports = {
+module.exports = {
 
     /**
     * Here your can configure your routes
@@ -6,32 +6,36 @@
     * Each route is identify by its path like /books/:id
     * Then this object can specify a method for the route --> Default GET
     * There must have an action on each route
-    * You can specify a controller where you action is define --> in ../ controllers directory
+    * You can specify a controller where you action is define --> in ../controllers directory
     * You can specify middleware to use
     * You can specify if this route must be authenticate
-    * You can specify is you want that this route render socket at the end.
+    * You can specify if you want that this route send socket.
     * You can spaecify necessary parameters
+    * You can specify what the user should have to be able to call the route
     **/
     
+    /* Work In Progress */
+    default : {
+        method : 'GET',
+        auth : true,
+        socket : true
+    },
+    /* Work In Progress */
+
+
     // REST API ---------------------------------------------------------------------
     // BASE ROUTE ======================= 
-
-    default : {
-
-    },
-
     '/' : {
         action : function(req, res) {
             res.json({message: 'Welcome on our Api', status : 200});
-        },
-        socket : true
+        }
     },
 
     // REQUEST AUTHENTCATION ROUTE ======================= 
-    '/authenticate' : {
+    '/login' : {
         method : 'POST',
         controller : 'authController',
-        action : 'generate',
+        action : 'login',
         middlewares : ['generateAuth'],
         parameters : ['username', 'password']
     },
@@ -45,24 +49,28 @@
         parameters : ['username', 'password']
     },
 
+    '/users/:id' : {
+        method : 'GET',
+        controller : 'userController',
+        action : 'get',
+        auth : true
+    }, 
+
     // // TODOS =======================  
     // GET ALL
     '/todos' : {
+        method : 'GET',
         controller : 'todoController',
         action : 'getAll',
-        socket : true,
-        auth : false
+        auth : true
     },
 
     // GET
-    '/todo/:id' : {
+    '/todos/:id' : {
+        method : 'GET',
         controller : 'todoController',
-        action : 'getAll',
-        socket : true,
-        auth : false
+        action : 'get',
+        auth : true
     }
-
-    // Generate CRUD for these ressources
-    // ressources : ['users', 'todos'] 
 
 }
