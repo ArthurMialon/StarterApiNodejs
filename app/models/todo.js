134983fa55cde365f41a080ab75460351f1ba04e
ArthurMialon <arthurmialon@gmail.com>
2015-05-06 12:16:36 +0200

    remove version key

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var todoSchema = mongoose.Schema({
     text        : String,
     done   	    : Boolean
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('Todo', todoSchema);
