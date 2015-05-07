db2b0217b4657a76e03007f19ee0fc4ff55bbbf6
ArthurMialon <arthurmialon@gmail.com>
2015-05-07 11:45:53 +0200

    function to object

--- app/core/parameters.js
+++ app/core/parameters.js
@@ -1,12 +1,18 @@
-module.exports = function(req, route) {
-  var parameters = route.parameters;
-  var params = [];
+module.exports = {
+	
+	/**
+	* Check necessary parameters
+	*/
+	check: function(req, route) {
+	  var parameters = route.parameters;
+	  var params = [];
 
-  if (parameters) {
-    for(var p in parameters) {
-      if(!req.body[parameters[p]]) { return false; }
-    }
-  }
+	  if (parameters) {
+	    for(var p in parameters) {
+	      if(!req.body[parameters[p]]) { return false; }
+	    }
+	  }
+	  return true;
+	}
 
-  return true;
 }
