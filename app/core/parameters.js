fb76206a1a287f9681da6075a866cb00b43efde6
ArthurMialon <arthurmialon@gmail.com>
2015-05-07 11:07:44 +0200

    add parameters modules

---
+++ app/core/parameters.js
@@ -0,0 +1,12 @@
+module.exports = function(req, route) {
+	var parameters = route.parameters;
+  var params = [];
+
+    if (parameters) {
+      for(var p in parameters) {
+        if(!req.body[parameters[p]]) { return false; }
+      }
+    }
+
+    return true;
+}
