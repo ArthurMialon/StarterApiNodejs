eccbeabe6de9e462efe9fe0005cd681b1d3566c6
ArthurMialon <arthurmialon@gmail.com>
2015-05-07 16:35:45 +0200

    update model todo

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,8 +1,22 @@
 var mongoose = require('mongoose');
 
 var todoSchema = mongoose.Schema({
-  text: String,
-  done: Boolean
+  text			: String,
+  done			: Boolean,
+  updated_at	: Date,
+  created_at	: Date
 }, { versionKey: false });
+
+todoSchema.pre('save', function(next) {
+  var todo = this;
+  var now = new Date();
+
+  // Add created at
+  todo.updated_at = now;
+  if (!todo.created_at)
+    todo.created_at = now;
+
+  next();
+});
 
 module.exports = mongoose.model('Todo', todoSchema);
