be92e183c37afe9f9619893913dd993d10cb7ab3
ArthurMialon <arthurmialon@gmail.com>
2015-05-07 11:45:40 +0200

    add is_js

--- app/core/need.js
+++ app/core/need.js
@@ -1,28 +1,59 @@
-module.exports = function(req, route) {
+var is = require('is_js');
 
-  // TODO
-  // =======
-  // TESTER FONCTION DU TYPE
-  // => BOOLEAN
-  // => STRING
-  // => INTEGER
-  // => FLOAT
-  // => DATE
+module.exports = {
 
-  var need = route.need;
-  var user = req.user;
+  /**
+  * Check what the user should be
+  */
+  check: function(req, route) {
+    // TODO
+    // =======
+    // TESTER FONCTION DU TYPE AVEC IS_JS
+    // => BOOLEAN
+    // => STRING
+    // => INTEGER
+    // => FLOAT
+    // => DATE
 
-  for(var n in need) {
-    if (typeof need[n] == 'boolean') {
-      // Is user can and if the variable exist
-      if (user[n] != need[n] && user[n])
-        return false;
-    }else {
-      // It's a string
-      if (user[n] != need[n] && user[n])
-        return false;
+    var need = route.need;
+    var user = req.user;
+
+    for(var n in need) {
+      if (typeof need[n] == 'boolean') {
+        // Is user can and if the variable exist
+        if (user[n] != need[n] && user[n])
+          return false;
+      }else {
+        // It's a string
+        if (user[n] != need[n] && user[n])
+          return false;
+      }
     }
+
+    return true;
+  },
+
+  /**
+  * Check boolean
+  */
+  isBoolean: function() {
+
+  },
+
+  isInteger: function() {
+
+  },
+
+  isString: function() {
+
+  },
+
+  isFloat: function() {
+
+  },
+
+  isDate: function() {
+
   }
 
-  return true;
 }
