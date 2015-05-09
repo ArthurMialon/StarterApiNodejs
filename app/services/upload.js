8da5964bb05f84940fa9a059c7cac20e558fa5ca
ArthurMialon <arthurmialon@gmail.com>
2015-05-09 19:12:50 +0200

    upload function

---
+++ app/services/upload.js
@@ -0,0 +1,19 @@
+var fs            = require('fs');
+var configuration = require('../config/configuration');
+var path          = './app/' + configuration.publicDir + '/' + configuration.upload.dir + '/';
+
+module.exports = function(files, name, callback) {
+  var file = files.file;  
+  var buffer = file.buffer;
+  var fileName = file.name;
+
+  var stream = fs.createWriteStream(path + fileName);
+  stream.write(buffer);
+  stream.on('error', function(err) {
+      callback(err, false);
+  });
+  stream.on('finish', function() {
+      callback(false, true);
+  });
+  stream.end();
+}
