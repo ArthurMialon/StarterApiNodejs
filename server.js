cbd90676fdf141f7e19ee909b4d9117a5fdecb5c
ArthurMialon <arthurmialon@gmail.com>
2015-05-09 19:13:15 +0200

    facto server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ var requester 		= require(corePath + 'requester');
 var database 		= require(corePath + 'database');
 var prefix 			= (configuration.apiPrefix) ? configuration.apiPrefix : '/';
 var http 			= require('http');			
-var pjson			= require('./package.json');		
-var port     		= process.env.PORT || 8080;          
+var port          	= process.env.PORT || 8080;
 var io 				= require('socket.io');
+var logger 			= require(corePath + 'logger');
 var server;
 
 // Configuration ==============================================
@@ -18,30 +18,15 @@ var server;
 // Database =================================================== 
 database(configuration);
 
-// Create server http =========================================
-server = http.createServer(app).listen(port);
-
-// Add Socket.io ==============================================
-io = io.listen(server);
-// Faire la connexion .on('connection') ici
-// Envoyer socket à router
-
-// Routing HTTP ===============================================
-requester(router, io);
+// Create server http  // Add Socket.io // Routing HTTP ===============================================
+requester(router, io.listen(http.createServer(app).listen(port)));
 
 // Adding prefix for the api ==================================
 app.use(prefix, router);
 
 // Routing Socket.io =======================================
 // require(configPath + 'routesSockets')(io.listen(server));
-
-// Log when launch on the server ==============================
-console.log('\x1b[34m%s\x1b[0m', "Server launch with StarterApiNodejs created by Arthur Mialon. :)");
-console.log('\x1b[32m%s\x1b[0m: ', "Your amazing app          :  " + pjson.name);
-console.log('\x1b[32m%s\x1b[0m: ', "Creating by               :  " + pjson.author);
-console.log('\x1b[32m%s\x1b[0m: ', "Runnig version            :  " + pjson.version);
-console.log('\x1b[32m%s\x1b[0m: ', configuration.messageOnConsole + port);
-
+logger();
 
 
 
