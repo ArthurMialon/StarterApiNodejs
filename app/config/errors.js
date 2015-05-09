ec2add019f1063194b515c3b2293c9a8e4938853
ArthurMialon <arthurmialon@gmail.com>
2015-05-09 19:10:44 +0200

    add error file

---
+++ app/config/errors.js
@@ -0,0 +1,18 @@
+modules.exports = {
+
+	'404' : {
+		code : 404,
+		message : 'Not found'
+	},
+
+	'401' : {
+		code 401,
+		message : 'Unauthorized'
+	},
+
+	500 : {
+		code : 500,
+		message : 'Internal server error'
+	}
+
+}
