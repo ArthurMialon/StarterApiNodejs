e1ca64966580c89c9098ec254442213eee37baf3
ArthurMialon <arthurmialon@gmail.com>
2015-05-09 19:12:38 +0200

    custom middleware

---
+++ app/middleware/custom.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+
+  logFinger: function(req, res, next) {
+
+    console.log("     __");
+    console.log("    (  \\ ");
+    console.log("     \\ =\\ ");
+    console.log("    __\\_'-\\ ");
+    console.log("  (____))( \\----");
+    console.log("  (____)) - ");
+    console.log("  (____))    ");
+    console.log("  (____))____/----");
+    console.log(" ______________________");
+    console.log("");
+    
+
+    next();
+  }
+}
