55d24c99575c42ca36169ba818cf76ea55534c8f
ArthurMialon <arthurmialon@gmail.com>
2015-05-09 19:11:35 +0200

    crud todo later

--- app/core/crud.js
+++ app/core/crud.js
@@ -1,22 +1,3 @@
 module.exports = {
-  get: function(req, res) {
-    // findOne
-  },
 
-  getAll: function(req, res) {
-    // Find ALl
-    res.json({message: 'get from the crud'});
-  },
-
-  post: function(req, res) {
-    // create
-  },
-
-  put: function(req, res) {
-    // Update
-  },
-
-  delete: function(req, res) {
-    // delete
-  }
 };
