fd7f04a5c3e6deb944281a8c0c125d1ec43eceee
ArthurMialon <arthurmialon@gmail.com>
2015-05-11 11:15:11 +0200

    create done/undo functions

--- app/models/todo.js
+++ app/models/todo.js
@@ -19,4 +19,37 @@ todoSchema.pre('save', function(next) {
   next();
 });
 
+todoSchema.statics.done = function done (id, cb) {
+  var self = this;
+  var conditions = { _id: id };
+  var update = { $set: { done: true }};
+  var options = {};
+
+  self.update(conditions, update, options, function(err, nb) {
+    if(err)
+      cb(err, false);
+    
+    self.findOne({_id : id}, function(err, todo) {
+      cb(err, todo);
+    });
+  });
+};
+
+todoSchema.statics.undo = function undo(id, cb) {
+  var self = this;
+  var conditions = { _id: id };
+  var update = { $set: { done: false }};
+  var options = {};
+
+  self.update(conditions, update, options, function(err, nb) {
+    if(err)
+      cb(err, false);
+    
+    self.findOne({_id : id}, function(err, todo) {
+      cb(err, todo);
+    });
+  });
+}
+
+
 module.exports = mongoose.model('Todo', todoSchema);
