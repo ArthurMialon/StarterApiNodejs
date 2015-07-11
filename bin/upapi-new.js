33e0971c8654a1f1fa1297679629d63f032d9467
Arthur Mialon <arthurmialon@gmail.com>
2015-07-11 23:42:46 +0200

    change logs:before and logs:after

--- bin/upapi-new.js
+++ bin/upapi-new.js
@@ -1,5 +1,6 @@
 var fs   = require('fs-extra');
 var path = require('path');
+var exec = require('child_process').exec;
 
 // Shorter functions
 var res = path.resolve;
@@ -7,8 +8,8 @@ var res = path.resolve;
 module.exports = {
   description: 'Create a new API.',
   logs: {
-    before: 'Creating your API.',
-    after: 'Your API is ready.'
+    before: 'All right ! We are creating your api !',
+    after: 'Your API is ready. Now use "cd blog" and run "npm install"'
   },
 
   init: function(root, userRoot, args) {
@@ -28,12 +29,11 @@ module.exports = {
     fs.copySync(res(root, '.gitignore'), res(appRoot, '.gitignore'));
     fs.copySync(res(root, '.npmignore'), res(appRoot, '.npmignore'));
     fs.copySync(res(root, 'package.json'), res(appRoot, 'package.json'));
-    fs.copySync(res(root, 'README.md'), res(appRoot, 'README.md'));
     fs.copySync(res(root, 'server.js'), res(appRoot, 'server.js'));
+    fs.copySync(path.join(root, 'lib'), path.join(appRoot, 'lib'));
 
     // Temporary
-    fs.copySync(path.join(root, 'lib'), path.join(appRoot, 'lib'));
-    fs.copySync(path.join(root, 'node_modules'), path.join(appRoot, 'node_modules'));
+    // fs.copySync(path.join(root, 'node_modules'), path.join(appRoot, 'node_modules'));
     fs.copySync(path.join(root, 'test'), path.join(appRoot, 'test'));
     fs.copySync(path.join(root, 'api'), path.join(appRoot, 'api'));
 
@@ -41,4 +41,3 @@ module.exports = {
     //        add an option so we can create an app with the todo and the tests
   }
 };
-
