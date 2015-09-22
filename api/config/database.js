142c2b7ae5f2207db8e65c856ad018639c6f9121
Arthur Mialon <arthurmialon@gmail.com>
2015-09-22 21:38:28 +0200

    Create Waterline management

--- api/config/database.js
+++ api/config/database.js
@@ -1,6 +1,9 @@
-// The framework uses the Waterline ORM
-// You can read the doc here
-// https://github.com/balderdashy/waterline
+/*---------------------------------------------/
+| Databases configuration
+| The framework uses the Waterline ORM
+| See documentation at:
+| https://github.com/balderdashy/waterline
+|----------------------------------------------*/
 
 // All your adapters
 let adapters = {
@@ -9,12 +12,12 @@ let adapters = {
 
 // All your databaes
 let connections = {
-    // Setup connections using the named adapter configs
+
     // Simple mongo connections
     myMongo: {
       adapter: 'mongo',
-      host: '127.0.0.1', // defaults to `localhost` if omitted
-      database: 'dragand' // or omit if not relevant
+      host: '127.0.0.1',
+      database: 'test'
     }
 
 };
