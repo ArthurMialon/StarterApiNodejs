41e3d08367b92f15ba4a0e33043a83d2d922d653
Arthur Mialon <arthurmialon@gmail.com>
2015-09-22 21:39:03 +0200

    Simple class User controller to handle some routes

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,19 +3,23 @@ import Controller from "../../lib/Controller";
 export default class User extends Controller {
 
   /**
-  *
+  * Constructor
   */
   constructor() {
     super();
+
+    // Load useful models
+    this.User = this.loadModel('user');
   }
 
   /**
   * Index route
+  * @param {object} req
+  * @param {object} res
+  * @param {function} next
   */
   index(req, res) {
-    res.send({
-      message: "coucou du controller"
-    });
+    res.send('ok');
   }
 
 }
