d5c92591253686c8c46f975ca6b1e7b33a27d922
Arthur Mialon <arthurmialon@gmail.com>
2015-10-31 07:55:09 +0100

    Add configuration

--- api/config/configuration.js
+++ api/config/configuration.js
@@ -1,8 +1,26 @@
 export default {
 
+  /*---------------------------------------------/
+  | Middleware configuration
+  | Here you can choose how to sort your Middleware
+  | and your options
+  |----------------------------------------------*/
+
+  sortMiddleware: [
+    "auth",
+    "need",
+    "middleware" // ==> Example: array middleware ["auth.generate"]
+  ], 
+
+
+  /*---------------------------------------------/
+  | Your secret key
+  | Use for authentication
+  |----------------------------------------------*/
+
   token: {
     secretkey: "mysupersecretkey"
-  }
+  },
 
 
 };
